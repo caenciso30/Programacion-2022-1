e845bb9c603a11bf6bf5c2c6605005f8fb1eac54
Live user for virtual machine <live@localhost.localdomain>
2022-08-22 12:41:07 -0500

    Suma para obtener pi

---
+++ 2022-08-22-Workshop/suma-pi.cpp
@@ -0,0 +1,22 @@
+// suma para calcular el numero pi: std::sqrt(6*result)
+// result = sum 1/n^2
+#include <iostream> // incluye utilidades para imprimir y leer de pantalla
+#include <cmath> // incluye funciones matematicas
+
+int main(int argc, char **argv)
+{
+  std::cout.setf(std::ios::scientific);
+  std::cout.precision(15);
+  double result = 0.0; // must be double to store floating point values
+  int n = 0;
+  const int nmax = 1000; // make this variable a constant one
+
+  // TODO: write a for loop performing the sum
+  for(n = 1; n <= nmax; n++){
+    result = result + 1.0/(n*n);
+  }
+  
+
+  std::cout << "The value of pi is: " << sqrt(6*result) << "\n";
+  return 0;
+}
